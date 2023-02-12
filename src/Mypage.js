0dcaaac43b7e71b240933fe1fb6faa483bb8df8a
Katsumi Nishimura <c0a21099e4@edu.teu.ac.jp>
2023-02-12 21:00:35 +0900

    project first_commit

---
+++ src/Mypage.js
@@ -0,0 +1,14 @@
+/* Mypage.js */
+
+import React from "react";
+
+const Mypage = () => {
+  return (
+    <>
+      <h1>マイページ</h1>
+      <button>ログアウト</button>
+    </>
+  );
+};
+
+export default Mypage;
---
+++ src/Register.js
@@ -0,0 +1,30 @@
+/* Register.js */
+
+import React from "react";
+
+const Register = () => {
+  return (
+    <>
+      <h1>新規登録</h1>
+      <form>
+        <div>
+          <label>メールアドレス</label>
+          <input
+            name="email"
+            type="email"
+          />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input
+            name="password"
+            type="password"
+          />
+        </div>
+        <button>登録する</button>
+      </form>
+    </>
+  );
+};
+
+export default Register;
